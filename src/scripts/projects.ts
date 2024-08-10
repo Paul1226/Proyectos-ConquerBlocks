86d90937cc3e50436dc23b46f080e5ceb960ac8b
Paul Guachichullca <paulguachichullca023@gmail.com>
2024-08-10 11:17:56 -0500

    Manejo de Transiciones y paginacion

---
+++ src/scripts/projects.ts
@@ -0,0 +1,94 @@
+export interface Project {
+    src: string;
+    alt: string;
+}
+
+export const headers: Project[] = [
+    {
+        src: "/images/headers/header01.jpg",
+        alt: "Preview image of the proyect header01",
+    },
+    {
+        src: "/images/headers/header02.jpg",
+        alt: "Preview image of the proyect header02",
+    },
+    {
+        src: "/images/headers/header03.jpg",
+        alt: "Preview image of the proyect header03",
+    },
+    {
+        src: "/images/headers/header04.jpg",
+        alt: "Preview image of the proyect header04",
+    },
+    {
+        src: "/images/headers/header05.jpg",
+        alt: "Preview image of the proyect header05",
+    },
+    {
+        src: "/images/headers/header06.jpg",    
+        alt: "Preview image of the proyect header06",
+    },
+    {
+        src: "/images/headers/header07.jpg",
+        alt: "Preview image of the proyect header07",
+    },
+    {
+        src: "/images/headers/header08.jpg",
+        alt: "Preview image of the proyect header08",
+    },
+    {
+        src: "/images/headers/header09.jpg",
+        alt: "Preview image of the proyect header09",
+    },
+    {
+        src: "/images/headers/header10.jpg",
+        alt: "Preview image of the proyect header10",
+    }
+]
+
+export const footers: Project[] =[
+    {
+        src: "/images/footers/footer001.jpg",
+        alt: "Preview image of the proyect footer01",
+    },
+    {
+        src: "/images/footers/footer002.jpg",
+        alt: "Preview image of the proyect footer02",
+    },
+    {
+        src: "/images/footers/footer003.jpg",
+        alt: "Preview image of the proyect footer03",
+    },
+    {
+        src: "/images/footers/footer004.jpg",
+        alt: "Preview image of the proyect footer04",
+    },
+    {
+        src: "/images/footers/footer005.jpg",
+        alt: "Preview image of the proyect footer05",
+    },
+    {
+        src: "/images/footers/footer006.jpg",
+        alt: "Preview image of the proyect footer06",
+    },
+]
+export const landings: Project[] =[
+    {
+        src: "/images/landings/landing01.jpg",
+        alt: "Preview image of the proyect landing01",
+    },
+    {
+        src: "/images/landings/landing02.jpg",
+        alt: "Preview image of the proyect landing02",
+    }
+]
+export const microsites: Project[] =[
+    {
+        src: "/images/microsites/microsite01.jpg",
+        alt: "Preview image of the proyect microsites01",
+    },
+    {
+        src: "/images/microsites/microsite002.jpg",
+        alt: "Preview image of the proyect microsites02",
+    }
+]
